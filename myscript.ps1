073e4f8b56fd3d28e972951f18fc23b1150179ae
George Wallace <georgewallace@users.noreply.github.com>
2019-05-02 12:25:22 -0700

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,2 +1,2 @@
-$info=$(Invoke-WebRequest "https://docs.microsoft.com/en-us/azure/automation/troubleshoot/runbooks").Content
+$info=$(Invoke-WebRequest "https://docs.microsoft.com/en-us/azure/automation/troubleshoot/runbooks" -usebasicparsing).Content
 write-output $info.Length.ToString()
