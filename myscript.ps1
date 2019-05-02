397e3ccd245070b98fc73f844d4133bcb7ecb59f
George Wallace <georgewallace@users.noreply.github.com>
2019-05-02 12:58:04 -0700

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,2 +1,3 @@
-$info=$(Invoke-WebRequest "https://docs.microsoft.com/en-us/azure/automation/troubleshoot/runbooks" -usebasicparsing).Content
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$info=$(Invoke-WebRequest "https://docs.microsoft.com/en-us/azure/automation/troubleshoot/runbooks").Content
 write-output $info.Length.ToString()
